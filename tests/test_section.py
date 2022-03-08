618a3bcac9699fdccf23caff4b97a53b2beea597
Christoph Goering <goering@imes.mavt.ethz.ch>
2022-03-08 16:09:05 +0100

    added tests

---
+++ tests/test_section.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import pytest
+import autoclasstoc
+
+
+class ExcludeSection1(autoclasstoc.Section):
+    key = 'dummy-section-1'
+    title = "Dummy Section 1:"
+    exclude_pattern = '__'
+
+
+class ExcludeSection2(autoclasstoc.Section):
+    key = 'dummy-section-2'
+    title = "Dummy Section 2:"
+    exclude_pattern = ['__', 'on']
+
+
+def test_ExcludeSection1():
+    names = {
+        '__init__': True,
+        '__len__': True,
+        'foo': False,
+        'bar': False,
+        't__t': True
+    }
+    cls = ExcludeSection1('state', 'cls')
+
+    for key, item in names.items():
+        assert item == cls.check_for_pattern(key)
+
+
+def test_ExcludeSection2():
+    names = {
+        '__init__': True,
+        '__len__': True,
+        'foo': False,
+        'bar': False,
+        't__t': True,
+        'on_off': True,
+        'out': False
+    }
+
+    cls = ExcludeSection2('state', 'cls')
+    for key, item in names.items():
+        assert item == cls.check_for_pattern(key)
